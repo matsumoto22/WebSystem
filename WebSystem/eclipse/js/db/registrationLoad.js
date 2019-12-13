e2904dbc3b2ef1cb0f04fb9c95ce36f71590a45c
matsumoto <b7325@oic.jp>
2019-12-13 18:01:37 +0900

    学科・コース管理

--- WebSystem/eclipse/js/db/registrationLoad.js
+++ WebSystem/eclipse/js/db/registrationLoad.js
@@ -13,11 +13,25 @@ $(document).ready( function(){
     		// 検索結果のデータをJson形式から配列に変換
         	let studentDate = JSON.parse(XHR.responseText);
 
+          var str = studentDate[0];
+
+          // 0文字目から4文字分を指定
+          let result = str.substr( 0, 1 );
+          let number = str.substr( 1, 2);
+
+          let a = Number(number) + 1;
+          a = String(a);
+
+          if(a.length == 1){
+            a = '0' + a;
+          }
+          a = result + a;
+
+          console.log( result );
+          console.log( a );
     		for(let i = 0; i < Object.keys(studentDate).length; i++){
-            $("#subject_id").val(studentDate[i]);
+            $("#subject_id").val(a);
         	}
     	};
-
 	}
-
 });
